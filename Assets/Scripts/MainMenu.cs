85fd28546db63d662e25f9b0602e6afd356661ac
DESKTOP-8EMJ2DT\usuario <meryetc@gmail.com>
2024-12-18 13:04:40 +0100

    merging with map

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SceneManager.LoadScene("Map", LoadSceneMode.Additive);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
